2f4ffa6cfde48fa3f0fd9cb0439b5ecc2d6f1c2d
didierganthier <didierganthierperan@gmail.com>
2020-08-20 10:06:22 -0400

    Appbar setup

---
+++ lib/movie_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  Color mainColor = const Color(0xff3C3261);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.3,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: Icon(
+          Icons.arrow_back,
+          color: mainColor,
+        ),
+        title: Text(
+          'Movies',
+          style: TextStyle(
+            color: mainColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Icon(
+            Icons.menu,
+            color: mainColor,
+          )
+        ],
+      ),
+    );
+  }
+}
