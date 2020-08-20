345802dc523f15d5f632dbc176fcbc5f97f58d6b
didierganthier <didierganthierperan@gmail.com>
2020-08-20 14:11:32 -0400

    movie_details.dart updated

--- lib/movie_details.dart
+++ lib/movie_details.dart
@@ -10,108 +10,113 @@ class MovieDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.network(
-            image_url + movie['poster_path'],
-            fit: BoxFit.cover,
-          ),
-          BackdropFilter(
-            filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-            child: Container(
-              color: Colors.black.withOpacity(0.5),
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.network(
+              image_url + movie['poster_path'],
+              fit: BoxFit.cover,
             ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Container(
-                    alignment: Alignment.center,
-                    child: Container(
-                      decoration: BoxDecoration(
+            BackdropFilter(
+              filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+              child: Container(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 400.0,
+                        height: 400.0,
+                        decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10.0),
                           image: DecorationImage(
-                            image:
-                                NetworkImage(image_url + movie['poster_path']),
+                            image: NetworkImage(image_url + movie['poster_path']),
                             fit: BoxFit.cover,
                           ),
                           boxShadow: [
                             BoxShadow(
-                                color: Colors.black,
-                                blurRadius: 20.0,
-                                offset: Offset(0.0, 10.0))
-                          ]),
-                    ),
-                  ),
-                  Container(
-                    margin:
-                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Text(
-                          movie['title'],
-                          style: TextStyle(color: Colors.white, fontSize: 30.0),
-                        )),
-                        Text(
-                          '${movie['vote_average']}/10',
-                          style: TextStyle(color: Colors.white, fontSize: 20.0),
-                        )
-                      ],
-                    ),
-                  ),
-                  Text(
-                    movie['overview'],
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.all(10.0)),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          width: 150.0,
-                          height: 60.0,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Rate Movie',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Color(0xaa3C3261),
-                          ),
+                              color: Colors.black,
+                              blurRadius: 20.0,
+                              offset: Offset(0.0, 10.0),
+                            )
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Container(
-                          padding: EdgeInsets.all(16.0),
-                          alignment: Alignment.center,
-                          child: Icon(
-                            Icons.bookmark,
-                            color: Colors.white,
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Color(0xaa3C3261)
+                    ),
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Text(
+                            movie['title'],
+                            style: TextStyle(color: Colors.white, fontSize: 30.0),
+                          )),
+                          Text(
+                            '${movie['vote_average']}/10',
+                            style: TextStyle(color: Colors.white, fontSize: 20.0),
+                          )
+                        ],
+                      ),
+                    ),
+                    Text(
+                      movie['overview'],
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(10.0)),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            width: 150.0,
+                            height: 60.0,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Rate Movie',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Color(0xaa3C3261),
+                            ),
                           ),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Container(
+                            padding: EdgeInsets.all(16.0),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.bookmark,
+                              color: Colors.white,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Color(0xaa3C3261),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
